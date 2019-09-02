783679111e2b944e10b3a95480280efa9520a195
Kim <ugnoeyh0390@gmail.com>
2019-09-03 02:41:36 +0900

    npm install error fix

--- install-prereqs-ubuntu.sh
+++ install-prereqs-ubuntu.sh
@@ -4,6 +4,8 @@ curl -O https://hyperledger.github.io/composer/latest/prereqs-ubuntu.sh
 chmod u+x ~/prereqs-ubuntu.sh
 ~/prereqs-ubuntu.sh
 
+export npm_config_prefix=~/.npm-prefix
+
 chmod u+x ./install-prereqs-ubuntu.sh ./install-dev-env.sh ./start-fab-dev-server.sh
 
 
